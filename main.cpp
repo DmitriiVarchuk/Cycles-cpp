1de247fc44b06141289030d290b1388272d5f16e
Dmytro Varchuk <loltop792@gmail.com>
2024-12-05 23:45:48 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;	
+
+int main() {
+    size_t sum = 0, a;
+    cout << "Enter a number: ";
+    cin >> a;
+
+    for (size_t i = a; i <= 500; i++) {
+        sum += i;
+    }
+
+    cout << "The sum from " << a << " to 500 is: " << sum << endl;
+    return 0;
+}
